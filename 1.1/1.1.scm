79beb7c0fb23011c8ea2d4f1d789d17373d55e50
Mote Mere <motemere@gmail.com>
2021-01-15 01:18:36 +0300

    codestyle

--- 1.1/1.1.scm
+++ 1.1/1.1.scm
@@ -1,7 +1,10 @@
 (define pi 3.14159)
 
-(define (circumference radius) (* 2 pi radius))
+(define (circumference radius) 
+  (* 2 pi radius))
 
-(define (square x) (* x x))
+(define (square x) 
+  (* x x))
 
-(define (sum-of-squares x y) (+ (square x) (square y)))
+(define (sum-of-squares x y) 
+  (+ (square x) (square y)))
